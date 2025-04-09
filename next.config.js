8f583e0fa3c772eb2c4360906121ec2c8d6d1a49
Jeethula <jeethula10@gmail.com>
2025-04-09 13:33:23 +0530

    Remove Next.js configuration file and update tsconfig to include it in the project

---
+++ next.config.js
@@ -0,0 +1,16 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   // output: 'export',
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // output: "export",
+    /* config options here */
+  };
+  
+  module.exports = nextConfig;
